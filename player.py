7cdf3f31bb10e9b99e6283d76116726910ec327e
mrbose <64681228+boseriddha@users.noreply.github.com>
2022-02-08 00:19:36 +0530

    Added the player class

---
+++ player.py
@@ -0,0 +1,13 @@
+"""
+This file contains the player class
+"""
+
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self):
+        pass
