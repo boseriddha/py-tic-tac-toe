f9f1cfaf350c0534536d0d6276c6832b694ffeb1
mrbose <64681228+boseriddha@users.noreply.github.com>
2022-02-08 00:24:58 +0530

    Added the Human Player class, and the Computer Player class. Added the get_move method for Human Player

--- player.py
+++ player.py
@@ -9,5 +9,32 @@ class Player:
     def __init__(self, letter):
         self.letter = letter
 
-    def get_move(self):
+    def get_move(self, game):
         pass
+
+class ComputerPlayer(Player):
+    def __init__(self, letter):
+        super.__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super.__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            # user input for the move
+            square = input(f"{self.letter}'s turn. Input move (0-8): ")
+            # check whether the move is valid or not
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid move, please try again!")
+        return val
