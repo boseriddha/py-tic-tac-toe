0f799827b21e5af51de12212f44c79dec3bbd9a9
mrbose <64681228+boseriddha@users.noreply.github.com>
2022-02-08 00:35:35 +0530

    Added the board build, and board printing methods to the game class

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 This is a tic-tac-toe game.
 """
 from player import Player, HumanPlayer, ComputerPlayer
+
+class TicTacToe:
+    def __init__(self):
+        # this is the main game board
+        self.board = self.make_board()
+        # this is the attribute holding the winner of a game instance
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return [' ' for i in range(9)]
+
+    @staticmethod
+    def print_nums_board():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('|' + '|'.join(row) + '|')
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('|' + '|'.join(row) + '|')
